cb36f214f1e9c55ab2e0b6f56605a1c2d0b7a633
Davis L <davis.lantz@gmail.com>
2019-04-03 16:13:10 -0700

    resolve conflicts between bstrap & local

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask  
+from flask import Flask , render_template,request,redirect
 app = Flask(__name__)  
 
 @app.route('/')          
-def hello_world():
+def render_index():
     
-    return 'Hello World!'  
+    return render_template('index.html')
 if __name__=="__main__":    
     app.run(debug=True)    
